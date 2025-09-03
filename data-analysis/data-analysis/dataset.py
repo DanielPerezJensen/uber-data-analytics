9668455115dcd1be3053dea2483dfae0c227e6a1
Daniel Perez Jensen <danielperezjensen@gmail.com>
2025-09-03 09:13:11 +0200

    feat: add dataset.py and utils.py providing logging

--- data-analysis/data-analysis/dataset.py
+++ data-analysis/data-analysis/dataset.py
@@ -1,43 +1,104 @@
+import datetime
 import os
+from typing import Optional
 
-# import pandas as pd
+import pandas as pd
+import utils
 from dotenv import load_dotenv
 from google.cloud import bigquery
 from loguru import logger
 
+utils.setup_logging("INFO")
 
-def read_data_from_bigquery(dataset_id, table_id):
+
+def read_data_from_file(
+    file_path: str,
+    start_datetime: Optional[datetime.datetime] = None,
+    end_datetime: Optional[datetime.datetime] = None,
+    datetime_format: str = "%Y-%m-%d %H:%M:%S",
+):
+    """Reads data from a CSV file and filters it based on the provided time range.
+
+    :param file_path: Path to the CSV file
+    :param start_time: start_time, defaults to None
+    :param end_time: end_time, defaults to None
+    :return: Filtered data
     """
-    Reads the contents of a file from GCS given its URI.
-    Returns the file content as bytes.
+
+    logger.info(f"Reading data from file: {file_path}")
+    df = pd.read_csv(file_path)
+
+    df["datetime"] = pd.to_datetime(df["Date"] + " " + df["Time"], format=datetime_format)
+
+    if start_datetime:
+        df = df[df["datetime"] >= start_datetime]
+    if end_datetime:
+        df = df[df["datetime"] <= end_datetime]
+
+    df.drop(columns=["datetime"], inplace=True)
+
+    logger.success(f"Data read successfully with {len(df)} records after filtering.")
+
+    return df
+
+
+def read_raw_data_from_bigquery(
+    start_datetime: Optional[datetime.datetime] = None,
+    end_datetime: Optional[datetime.datetime] = None,
+):
+    """Reads data from a BigQuery table and filters it based on the provided time range.
+
+    :param start_datetime: Start datetime for filtering, defaults to None
+    :param end_datetime: End datetime for filtering, defaults to None
+    :return: Filtered data
     """
 
-    bigquery_client = bigquery.Client()
-    table_ref = bigquery_client.dataset(dataset_id).table(table_id)
+    load_dotenv()  # Load environment variables from .env file
 
-    try:
-        table = bigquery_client.get_table(table_ref)
-        rows = bigquery_client.list_rows(table)
-        dataframe = rows.to_dataframe()
-        logger.info(f"Successfully read {len(dataframe)} rows from BigQuery table.")
-        print(dataframe.head())  # Display the first few rows of the dataframe
-        return dataframe
-    except Exception as e:
-        logger.error(f"Error reading data from BigQuery: {e}")
-        raise
+    bigquery_staging_data_table = os.getenv("BQ_STAGING_DATA_TABLE")
+
+    if not bigquery_staging_data_table:
+        logger.error("Environment variable BQ_STAGING_DATA_TABLE is not set.")
+        raise ValueError("Environment variable BQ_STAGING_DATA_TABLE is not set.")
+
+    logger.info(f"Reading data from BigQuery table: {bigquery_staging_data_table}")
+
+    client = bigquery.Client()
+
+    query = f"""
+    SELECT
+        *
+    FROM (
+        SELECT
+        *,
+        PARSE_DATETIME('%Y-%m-%d %H:%M:%S', CONCAT(Date, ' ', Time)) AS datetime
+        FROM
+        `{bigquery_staging_data_table}`
+    )
+    WHERE
+        (datetime >= @start_datetime OR @start_datetime IS NULL)
+        AND (datetime <= @end_datetime OR @end_datetime IS NULL)
+    """
+
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("start_datetime", "DATETIME", start_datetime),
+            bigquery.ScalarQueryParameter("end_datetime", "DATETIME", end_datetime),
+        ]
+    )
+
+    logger.debug(f"Executing query: {query}")
+
+    df = client.query(query, job_config=job_config).to_dataframe()
+
+    logger.success(f"Data read successfully with {len(df)} records after filtering.")
+
+    return df
 
 
-# --- Example Usage ---
 if __name__ == "__main__":
-    # Replace with your specific details
-    load_dotenv()
+    df = read_data_from_file(
+        "data/raw/ncr_ride_bookings.csv",
+    )
 
-    dataset_id = os.getenv("BQ_DATASET")
-    table_id = "staging"
-
-    if not all([dataset_id, table_id]) and not all(isinstance(var, str) for var in [dataset_id, table_id]):
-        raise ValueError(
-            f"One or more required env vars are missing or not strings: dataset_id={dataset_id}, table_id={table_id}"
-        )
-
-    df = read_data_from_bigquery(dataset_id, table_id)
+    df = read_raw_data_from_bigquery()
